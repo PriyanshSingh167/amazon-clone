c22cc44278f6150d7db2c5cf7a84bb0d510d50ff
priyanshsingh <priyansh@singh.com>
2022-07-04 22:46:17 +0530

    Registering user using firebase

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "../styles/Login.css";
+import { Link, useNavigate } from "react-router-dom";
+// import { toast } from 'react-toastify'
 import Button from "@mui/material/Button";
+import "../styles/Login.css";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+
+import { db } from "../firebase.config";
+
 function SignUp() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     password: "",
   });
+  const { name, email, password } = formData;
 
-  const { email, password, name } = formData;
+  const navigate = useNavigate();
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -19,51 +28,86 @@ function SignUp() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      // const formDataCopy = { ...formData }
+      // delete formDataCopy.password
+      // formDataCopy.timestamp = serverTimestamp()
+
+      // await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="login">
-      <Link to="/">
-        <img
-          className="header__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-          alt="amazon logo"
-        />
-      </Link>
-      <div className="login__container">
-        <h1>Sign-Up</h1>
-        <form action="">
-          <h5>Name</h5>
-          <input
-            type="text"
-            className="nameInput"
-            id="name"
-            value={name}
-            onChange={onChange}
+    <>
+      <div className="login">
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+            alt="amazon logo"
           />
-          <h5>E-mail</h5>
-          <input type="email" value={email} onChange={onChange} id="email" />
-          <h5>Password</h5>
-          <input
-            type="password"
-            value={password}
-            id="password"
-            onChange={onChange}
-          />
-        </form>
-        <p>
-          By continuing, you agree to Amazon's Conditions of Use and Privacy
-          Notice.
-        </p>
-        <Link to="/sign-up">
-          <Button
-            variant="contained"
-            className="login__signInButton"
-            // onClick={signUp}
-          >
-            Sign-up
-          </Button>
         </Link>
+        <div className="login__container">
+          <form onSubmit={onSubmit}>
+            <h1>Sign-Up</h1>
+            <h5>Name</h5>
+            <input
+              type="text"
+              className="nameInput"
+              placeholder="Name"
+              id="name"
+              value={name}
+              onChange={onChange}
+            />
+            <h5>Email</h5>
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <h5>Password</h5>
+            <input
+              type="password"
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <button className="login__signInButton">Sign-Up</button>
+          </form>
+
+          <Link to="/login" className="registerLink">
+            Sign In Instead
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
